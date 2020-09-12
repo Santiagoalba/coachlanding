6caf3ce837b74719d8a83654b442ad724dfd9892
Santiagoalba <Sanalba2222@gmail.com>
2020-09-11 23:35:52 -0300

    login signup etc

--- js/main.js
+++ js/main.js
@@ -1,25 +1,156 @@
 home = {
 
+    // validarNick(data) {
+    //     var flag = true;
+    //     if(!/^[A-Za-z]+$/.test(data)){
+    //         console.log("nick invalido")
+    //         var flag = false;
+    //     }
+    //     return flag;
+    // },
 
-    validateEmail(data) {
-        var flag = true;    
-         if (!/[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/.test(data)) {
-             console.log("Esta mal");
-            flag = false;
-         }
+
+    // validarMail(data) {
+    //     var flag = true;    
+    //      if (!/[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/.test(data)) {
+    //          console.log("email invalido");
+    //         flag = false;
+    //      }
          
-       return flag;
-      },
+    //    return flag;
+    //   },
+
+    // validarPass(data){
+    //     var flag = true
+    //    if (!/^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]){8,15}$/.test(data)){
+    //        console.log("pass invalida")
+    //        flag = false
+    //    }
+    //    return flag
+    // },
 
      
-    validar(){
-        let email = document.getElementById("newsletterEmail");
-        if (home.validateEmail(email.value)){
-            console.log("email valido")
-        }
-    }
+    // validar(){
+    //     let email = document.getElementById("email");
+    //     if (home.validarMail(email.value)){
+    //         console.log("email valido")
+    //     }
+    //     let nick = document.getElementById("nick")
+    //     if (home.validarNick(nick.value)){
+    //         console.log("nick valido")
+    //     }
+    //     let pass = document.getElementById("pass")
+    //     if (home.validarPass(pass.value)){
+    //         console.log("pass valida")
+    //     }
+    // },
+
+    showPopUp(){
+        document.querySelector(".signInPopUp").setAttribute("class", "signInPopUp show")
+    },
+
+    showPopUpLog(){
+        document.querySelector(".logInPopUp").setAttribute("class", "logInPopUp show")
+    },
+
+    
+    
+    hidePopUp(){
+        document.querySelector(".signInPopUp").setAttribute("class", "signInPopUp")
+    },
+    
+    hidePopUpLog(){
+        document.querySelector(".logInPopUp").setAttribute("class", "logInPopUp")
+    },
+    
+    clickOutside(){
+        let abc = document.querySelector('.logInPopUp')
+        abc.addEventListener('click', function(e){ 
+        if (document.querySelector('.logInForm').contains(e.target)){
+        console.log("adentro") 
+    } else{ 
+        home.hidePopUpLog()
+        // Clicked outside the box
+        console.log("afuera")
+        } 
+    });
+    },
+    toLogIn(){
+        document.querySelector(".logInPopUp").setAttribute("class", "logInPopUp show")
+        home.hidePopUp()
+    },
+
+    toSignUp(){
+        document.querySelector(".signInPopUp").setAttribute("class", "signInPopUp show")
+        home.hidePopUpLog()
+    },
+    
 
 }
+
+home.clickOutside()
+
+const nick = document.getElementById('nick')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (nick.value === '' || nick.value == null) {
+    messages.push('Nick is required')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters')
+  }
+
+  if (/^[a-zA-Z0-9- ]*$/.test(password.value) === false) {
+      messages.push('Password must only contain numbers and letters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (password2.value !== password.value) {
+      messages.push('Repeat password was not the same as password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
+
+
+newsSubscriber = document.getElementById("newsletter")
+newsEmail = document.getElementById("newsletterEmail")
+
+let subscribedNews = [
+    "santiago@gmail.com",
+    "pedro@gmail.com",
+]
+
+
+newsSubscriber.addEventListener('submit', () => {
+    
+    subscribedNews.push(newsEmail.value)
+    console.log(subscribedNews, "puto")
+
+})
+
+console.log(subscribedNews, "hola")
+
+
+
+
 
 
 let coachsData = [
@@ -173,6 +304,7 @@ displayArray = [];
 
         }).join('')}
     `
+
 
     
 
